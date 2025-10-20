d446c7fb491f831d3b8a9eb5fa361f2e209f69ec
Francisco <franciscogbonorino@gmail.com>
2025-10-20 19:45:32 -0300

    Testeo de Monitoreo 8

--- myproject/app.py
+++ myproject/app.py
@@ -106,5 +106,5 @@ def health():
 
 # DADASDSAD
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1000)
-
+    port = int(os.environ.get("PORT", 1000))
+    app.run(host="0.0.0.0", port=port)
